c8803c18873856bf8f62ce3f16c0f847598d82b3
Anthony Wang <32027606+Honyant@users.noreply.github.com>
2023-08-21 14:52:40 -0500

    Update txt2cmd.py

--- txt2cmd.py
+++ txt2cmd.py
@@ -1,28 +1,14 @@
 import sys
 import openai
 openai.api_key = "INSERT_KEY"
-#credits for prompt: some guy on discord
 
-response = openai.Completion.create(engine="code-davinci-002", prompt="""#!/bin/bash
 
-# List of one-liner shell commands for Arch.
-# Language: Bash
-# Operating System: INSERT_OS_HERE
-
-# Get my IP
-dig +short myip.opendns.com @resolver1.opendns.com
-
-# Print the current directory
-pwd
-
-# List files
-ls -l
-
-# Find every file that is not .mp3
-find . -name '*' -type f -not -path '*.mp3'
-
-# Change directory to /tmp
-cd /tmp
-
-# """ + " ".join(sys.argv[1:]) + "\n", max_tokens=100, temperature=0, stop="\n")
-print(response.choices[0].text)
+MODEL = "gpt-3.5-turbo"
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "Give bash commands on mac for given natural language prompts. Do not output ANYTHING other than the pure bash command requested!"},
+        {"role": "user", "content": f"{sys.argv[1:]}"}
+    ],
+    temperature=0)
+print(response['choices'][0]['message']['content'])
